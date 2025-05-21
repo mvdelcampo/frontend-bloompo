03c95cc61432aaa778ab923b9a0f7d1dab33988b
valentinagonzalez21 <vgonzalez@correo.um.edu.uy>
2025-05-21 19:39:44 -0300

    create project structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,36 @@
-import { Stack } from "expo-router";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+    const isLoggedIn = true;
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+         {!isLoggedIn ? (
+        <Stack.Screen name="(auth)" />
+      ) : (
+        <>
+          <Stack.Screen name="(tabs)" />
+        </>
+      )}
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
 }
